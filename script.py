12ff21f2191426138532f21281085297fe32d954
이동현 <mallak5335@naver.com>
2022-08-05 14:04:09 +0900

    snake case to camel case in class name

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ import sys
 
 path = sys.argv[1]
 file_name = sys.argv[2]
+init, *temp = sys.argv[2].split('_')
+class_name = ''.join([init.capitalize(), *map(str.title, temp)])
 
 os.chdir(path)
 os.mkdir(file_name)
@@ -11,12 +13,12 @@ os.chdir(file_name)
 with open(f'{file_name}_binding.dart', 'w') as f:
     f.write(f"import 'package:fit_seoul/modules/{file_name}/{file_name}_controller.dart';\n")
     f.write("import 'package:get/get.dart';\n")
-    f.write(f"class {file_name.capitalize()}binding extends GetxController {{\n")
+    f.write(f"class {class_name}binding extends GetxController {{\n")
     f.write("@override\n")
     f.write("List<Bind> dependencies() {\n")
     f.write("return [\n")
-    f.write(f"Bind.lazyPut<{file_name.capitalize()}Controller>(\n")
-    f.write(f"() => {file_name.capitalize()}Controller(),\n")
+    f.write(f"Bind.lazyPut<{class_name}Controller>(\n")
+    f.write(f"() => {class_name}Controller(),\n")
     f.write(")")
     f.write("];\n")
     f.write("}\n")
@@ -25,12 +27,12 @@ with open(f'{file_name}_binding.dart', 'w') as f:
 
 with open(f'{file_name}_controller.dart', 'w') as f:
     f.write("import 'package:get/get.dart';\n")
-    f.write(f"class {file_name.capitalize()}Controller extends GetxController {{}}\n")
+    f.write(f"class {class_name}Controller extends GetxController {{}}\n")
 
 with open(f'{file_name}_view.dart', 'w') as f:
     f.write(f"import 'package:fit_seoul/modules/{file_name}/{file_name}_controller.dart';\n")
     f.write("import 'package:flutter/material.dart';\n")
     f.write("import 'package:get/get.dart';\n")
-    f.write(f"class {file_name.capitalize()}View extends GetView<{file_name.capitalize()}Controller> {{\n")
+    f.write(f"class {class_name}View extends GetView<{class_name}Controller> {{\n")
     f.write("@override\n")
     f.write("Widget build(BuildContext context) {\nreturn Scaffold();\n}\n}\n")
